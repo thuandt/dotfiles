0f6a6271f0b07539eaa87a78ce20000693f7c1b9
MrTux <mrtux@ubuntu-vn.org>
2014-01-13 12:03:24 +0700

    rewrite install script

--- install.sh
+++ install.sh
@@ -2,17 +2,30 @@
 # Filename: install.sh
 # Last Modified: Mon 07 Jan 2013 01:07:00 AM ICT
 
-cp bash/bashrc ~/.bashrc
-cp bash/bash_logout ~/.bash_logout
-cp bash/bash_profile ~/.bash_profile
-mkdir -p ~/.bin && cp bin/* ~/.bin
-cp screen/screenrc ~/.screenrc
-cp shell/aliases ~/.shell_aliases
-cp shell/dir_colors ~/.dir_colors
-cp shell/env ~/.shell_env
-cp shell/functions ~/.shell_functions
-cp tmux/tmux.conf ~/.tmux.conf
-cp zsh/zlogout ~/.zlogout
-cp zsh/zprofile ~/.zprofile
-cp zsh/zshrc ~/.zshrc
-cp zsh/zshenv ~/.zshenv
+DOTFILES=`pwd`
+
+# install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# common shell
+ln -s "$DOTFILES/shell/aliases" ~/.shell_aliases
+ln -s "$DOTFILES/shell/dir_colors" ~/.dir_colors
+ln -s "$DOTFILES/shell/env" ~/.shell_env
+ln -s "$DOTFILES/shell/functions" ~/.shell_functions
+
+# bash
+ln -s "$DOTFILES/bash/bashrc" ~/.bashrc
+ln -s "$DOTFILES/bash/bash_logout" ~/.bash_logout
+ln -s "$DOTFILES/bash/bash_profile" ~/.bash_profile
+
+# zsh
+ln -s "$DOTFILES/zsh/zlogout" ~/.zlogout
+ln -s "$DOTFILES/zsh/zprofile" ~/.zprofile
+ln -s "$DOTFILES/zsh/zshrc" ~/.zshrc
+ln -s "$DOTFILES/zsh/zshenv" ~/.zshenv
+mkdir -p ~/.oh-my-zsh/custom/themes
+ln -s "$DOTFILES/zsh/mrtux.zsh-theme" ~/.oh-my-zsh/custom/themes
+
+# install some config file
+ln -s "$DOTFILES/screen/screenrc" ~/.screenrc
+ln -s "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
