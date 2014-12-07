364912fe13c3f35ae9488bcb3ed5865b6168ad0f
MrTux <mrtux@ubuntu-vn.org>
2014-12-08 02:13:41 +0700

    Using local copy instead of symlink

--- install.sh
+++ install.sh
@@ -8,24 +8,24 @@ DOTFILES=`pwd`
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # common shell
-ln -s "$DOTFILES/shell/aliases" ~/.shell_aliases
-ln -s "$DOTFILES/shell/dir_colors" ~/.dir_colors
-ln -s "$DOTFILES/shell/env" ~/.shell_env
-ln -s "$DOTFILES/shell/functions" ~/.shell_functions
+cp "$DOTFILES/shell/aliases" ~/.shell_aliases
+cp "$DOTFILES/shell/dircolors/dircolors.256dark" ~/.dircolors.256dark
+cp "$DOTFILES/shell/env" ~/.shell_env
+cp "$DOTFILES/shell/functions" ~/.shell_functions
 
 # bash
-ln -s "$DOTFILES/bash/bashrc" ~/.bashrc
-ln -s "$DOTFILES/bash/bash_logout" ~/.bash_logout
-ln -s "$DOTFILES/bash/bash_profile" ~/.bash_profile
+cp "$DOTFILES/bash/bashrc" ~/.bashrc
+cp "$DOTFILES/bash/bash_logout" ~/.bash_logout
+cp "$DOTFILES/bash/bash_profile" ~/.bash_profile
 
 # zsh
-ln -s "$DOTFILES/zsh/zlogout" ~/.zlogout
-ln -s "$DOTFILES/zsh/zprofile" ~/.zprofile
+cp "$DOTFILES/zsh/zlogout" ~/.zlogout
+cp "$DOTFILES/zsh/zprofile" ~/.zprofile
 cp "$DOTFILES/zsh/zshrc" ~/.zshrc
-ln -s "$DOTFILES/zsh/zshenv" ~/.zshenv
+cp "$DOTFILES/zsh/zshenv" ~/.zshenv
 mkdir -p ~/.oh-my-zsh/custom/themes
-ln -s "$DOTFILES/zsh/mrtux.zsh-theme" ~/.oh-my-zsh/custom/themes
+cp "$DOTFILES/zsh/mrtux.zsh-theme" ~/.oh-my-zsh/custom/themes
 
 # install some config file
-ln -s "$DOTFILES/screen/screenrc" ~/.screenrc
-ln -s "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
+cp "$DOTFILES/screen/screenrc" ~/.screenrc
+cp "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
