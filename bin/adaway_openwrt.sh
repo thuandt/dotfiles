2338cc19c9e07ef0d19ebb8d77b1089e73c70e0a
MrTux <mrtux@ubuntu-vn.org>
2018-12-05 23:55:04 +0700

    Add openwrt script for generate hosts file

--- bin/adaway_openwrt.sh
+++ bin/adaway_openwrt.sh
@@ -1,25 +1,27 @@
-#!/usr/bin/env bash
-# Filename: install-adblocking-hosts
+#!/bin/sh
+# Filename: update_script.sh
 # Description: Script to automate building an adblocking hosts file
 
 # Perform work in temporary files
-temphosts=$(mktemp)
-systemhosts="/etc/hosts"
-lastupdate=$(date)
+adaway_hosts="/etc/adway/hosts"
+last_update=$(date)
 
 # Adway lists
-adaway_list=(
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-              "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
-            )
+adaway_list="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+             https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
+
+# Replace old adway hosts file
+rm -vf "${adaway_hosts}"
+echo "# There is no place like 127.0.0.1" >> "${adaway_hosts}"
+echo "# Last update at ${last_update}" >> "${adaway_hosts}"
 
 # Obtain various hosts files and merge into one
 echo "Downloading adaway list ..."
-for i in "${adaway_list[@]}"
+for src in ${adaway_list}
 do
-   wget -q -O - "$i" >> "$temphosts"
+   echo "Download from $src"
+   wget -q --no-check-certificate -O - "$src" >> "${adaway_hosts}"
 done
-
 
 # Processing hosts file:
 # 1. Remove MS-DOS carriage returns
@@ -31,19 +33,13 @@ done
 # 5. Delete any comments on lines
 # 5. Convert tabs to spaces
 # 6. Remove extra spaces
+# 7. Remove sentry.io, ipinfo.io and fonts.googleapis.com (googleadapis.l.google.com)
 echo "Parsing and merging hosts files..."
 sed -i -e 's/\r//' -e 's/127.0.0.1/0.0.0.0/' -e '/^0.0.0.0/!d' \
     -e '/localhost/d' -e 's/#.*$//' -e 's/\t/ /g' -e 's/  */ /g' \
     -e '/sentry.io/d' -e '/ipinfo.io/d' -e '/googleadapis.l.google.com/d' \
-    "$temphosts"
+    "${adaway_hosts}"
 
-# Combine system hosts with adblocks
-echo "Merging with original system hosts..."
-# Delete all line from mark line (old data)
-sed -i '/There is no place like 127.0.0.1/,$d' $systemhosts
-# Add new line for next update
-echo "# There is no place like 127.0.0.1" >> $systemhosts
-echo "# Last update at $lastupdate" >> $systemhosts
-
-# Remove duplicate using sort and add them to system hosts file
-sort -u "$temphosts" >> "$systemhosts"
+# delete duplicate, consecutive lines from a file (emulates "uniq").
+# First line in a set of duplicate lines is kept, rest are deleted.
+sed -i '$!N; /^\(.*\)\n\1$/!P; D' "${adaway_hosts}"
