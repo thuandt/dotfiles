f3d4f471a1965095e6b28d04aa3bd97b8111bbbf
Thuan Duong <847996+thuandt@users.noreply.github.com>
2026-01-05 01:58:47 +0700

    add customize helm plugin
    Fix https://github.com/helm/helm/issues/5046

---
+++ zsh/omz/plugins/helm/helm.plugin.zsh
@@ -0,0 +1,7 @@
+# Autocompletion for helm.
+#
+# Copy from kubectl : https://github.com/pstadler
+
+if [ $commands[helm] ]; then
+    source <(helm completion zsh | sed -E 's/\["(.+)"\]/\[\1\]/g')
+fi
