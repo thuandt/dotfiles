3f6b11d6dbab7f0b80c3845f130a9efdca0a3c11
Thuan Duong <847996+thuandt@users.noreply.github.com>
2026-01-05 01:58:44 +0700

    Add OMZ customize plugins. ZSH: use custom omz path

---
+++ zsh/omz/plugins/gcloud/gcloud.plugin.zsh
@@ -0,0 +1,5 @@
+# Google Cloud SDK
+# The next line updates PATH for the Google Cloud SDK.
+source '/home/mrtux/Applications/google-cloud-sdk/path.zsh.inc'
+# The next line enables zsh completion for gcloud.
+source '/home/mrtux/Applications/google-cloud-sdk/completion.zsh.inc'
---
+++ zsh/omz/plugins/systemd/systemd.plugin.zsh
@@ -0,0 +1,21 @@
+user_commands=(
+  list-units is-active status show help list-unit-files
+  is-enabled list-jobs show-environment cat list-timers)
+
+dash_user_commands=(
+  start stop restart status enable disable edit cat daemon-reload)
+
+sudo_commands=(
+  start stop reload restart try-restart isolate kill
+  reset-failed enable disable reenable preset mask unmask
+  link load cancel set-environment unset-environment
+  edit daemon-reload)
+
+for c in $user_commands; do; alias sc-$c="systemctl $c"; done
+for c in $dash_user_commands; do; alias sc-user-$c="systemctl --user $c"; done
+for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
+
+alias sc-enable-now="sc-enable --now"
+alias sc-disable-now="sc-disable --now"
+alias sc-mask-now="sc-mask --now"
+alias sc-user-dr="systemctl --user daemon-reload"
